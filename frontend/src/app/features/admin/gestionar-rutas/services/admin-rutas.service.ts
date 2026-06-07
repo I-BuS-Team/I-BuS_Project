64a40f5176b3499348841912d847c6f0629f652b
Laura Daniela Guevara <laura.guevara02@uptc.edu.co>
2026-06-06 23:03:22 -0500

    Configuración front con back para el despliegue

--- frontend/src/app/features/admin/gestionar-rutas/services/admin-rutas.service.ts
+++ frontend/src/app/features/admin/gestionar-rutas/services/admin-rutas.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { API_URL } from '../../../../config';
 
 export interface Barrio {
   id?: number;
@@ -76,7 +77,7 @@ export interface Usuario {
   providedIn: 'root'
 })
 export class AdminRutasService {
-  private apiUrl = 'http://localhost:8000/api';
+  private apiUrl = API_URL;
 
   constructor(private http: HttpClient) {}
 
